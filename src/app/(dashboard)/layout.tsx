e256f29e275d8e7b57ece7eb2eb17d1a95db98d0
Alex Volynsky <alexvolynsky@Alexs-MacBook-Pro.local>
2026-02-11 22:59:45 -0500

    Design considerations

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
       <DashboardNav />
       <div className="flex">
         <DashboardSidebar />
-        <main className="relative z-10 flex-1 min-w-0 px-4 sm:px-6 lg:px-8 lg:pr-12 pb-24 lg:pb-8 pt-20 lg:pt-20 lg:ml-60 max-w-[1400px]">
+        <main className="relative z-10 flex-1 min-w-0 px-4 sm:px-6 lg:px-10 lg:pr-[12%] pb-24 lg:pb-8 pt-20 lg:pt-20 lg:ml-60">
           {children}
         </main>
       </div>
